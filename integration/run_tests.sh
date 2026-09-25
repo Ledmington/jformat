9224bc84227c9ccfdf95cd63b9aefedbde348913
Ledmington <ledmington.dev@gmail.com>
2026-09-25 11:33:01 +0200

    Added integration tests infrastructure

---
+++ integration/run_tests.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+die() {
+  echo "$*" >&2
+  exit 255
+}
+
+if [[ $# -ne 1 ]] ; then
+  die "Expected the jar file to run tests against."
+fi
+
+if [[ -n "${JAVA_HOME}" ]] ; then
+  JAVA=$(realpath "${JAVA_HOME}/bin/java")
+  if [[ ! -e ${JAVA} ]] ; then
+    die "JAVA_HOME was set but could not find 'java' executable."
+  fi
+else
+  echo "JAVA_HOME not set."
+  JAVA=$(realpath "$(which java)")
+  if [[ ! -e ${JAVA} ]] ; then
+    die "Could not find 'java' executable."
+  fi
+fi
+
+echo "Using java at '${JAVA}'"
+${JAVA} --version
+echo ""
+
+JAR_FILE=$1
+INTEGRATION_TEST_DIR="$(dirname "${BASH_SOURCE[0]}")"
+TEST_CASES=$(find "${INTEGRATION_TEST_DIR}" -mindepth 1 -maxdepth 1 -type d -printf "%f\n" | sort)
+for test_dir in ${TEST_CASES} ; do
+  OUTPUT="$(mktemp)"
+  INPUT="${INTEGRATION_TEST_DIR}/${test_dir}"
+
+  echo " - ${test_dir}..."
+  ${JAVA} -jar "${JAR_FILE}" "${INPUT}/input" "${OUTPUT}"
+  diff "${OUTPUT}" "${INPUT}/expected"
+  echo -e " - ${test_dir} \e[32mOK\e[0m"
+done
+
+echo "All tests passed!"
